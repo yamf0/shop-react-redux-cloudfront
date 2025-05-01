40d3485fe979d411ab5161f8c3e406ed9dcb4a7e
yamf0 <13.ymartinez.2b@gmail.com>
2025-05-01 12:15:56 -0600

    modified url to hit api gateway

--- src/queries/products.ts
+++ src/queries/products.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 import API_PATHS from "~/constants/apiPaths";
-import { AvailableProduct } from "~/models/Product";
+import {AvailableProduct, Product} from "~/models/Product";
 import { useQuery, useQueryClient, useMutation } from "react-query";
 import React from "react";
 
@@ -9,7 +9,7 @@ export function useAvailableProducts() {
     "available-products",
     async () => {
       const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.bff}/product/available`
+        `${API_PATHS.bff}/products`
       );
       return res.data;
     }
